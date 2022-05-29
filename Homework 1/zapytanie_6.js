b54f0fea741baf2b3c74111dfcab8377b7bb32de
Valerie <Valerie@MacBook-Pro-test.local>
2022-05-29 22:04:10 +0200

    homework 1 task 6

---
+++ Homework 1/zapytanie_6.js
@@ -0,0 +1,27 @@
+db.people.insert({
+    "sex" : "Female",
+    "first_name" : "Waleria",
+    "last_name" : "Sztankiewicz",
+    "job" : "QA Engineer",
+    "email" : "maila_nie_podam@gmail.com",
+    "location" : {
+        "city" : "Warszawa",
+        "address" : {
+            "streetname" : "Jana Pawła II",
+            "streetnumber" : "2"
+        }
+    },
+    "description" : "lorem ipsum 1",
+    "height" : "178.00",
+    "weight" : "60.00",
+    "birth_date" : "1996-05-25T12:10:58Z",
+    "nationality" : "Ukraine",
+    "credit" : [
+        {
+            "type" : "ing",
+            "number" : "1234555327",
+            "currency" : "PLN",
+            "balance" : "0.0"
+        }
+    ]
+})
